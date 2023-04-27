917afa1cb982a7b23848384d3917a6e7d88e9f30
santi <santi.ledes.99@gmail.com>
2023-04-27 09:01:21 -0300

    add choose and send actions

--- src/main/java/org/sfile/Main.java
+++ src/main/java/org/sfile/Main.java
@@ -5,12 +5,13 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.File;
+import java.io.*;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        File fileToSend;
+        // should be final because it will need to be accessed from an inner class
+        final File[] fileToSend = new File[1];
 
         JFrame frame = new JFrame("Client");
         frame.setSize(450, 450);
@@ -41,6 +42,46 @@ public class Main {
         btnContainer.add(send);
         btnContainer.add(choose);
 
+        choose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setToolTipText("Choose a file to send");
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    fileToSend[0] = chooser.getSelectedFile();
+                    fileName.setText("The file you want to send is: " + fileToSend[0].getName());
+                }
+            }
+        });
 
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (fileToSend[0] == null) {
+                    fileName.setText("Please choose a file first");
+                } else {
+                    try {
+                        FileInputStream input = new FileInputStream(fileToSend[0].getAbsolutePath());
+                        Socket socket = new Socket("localhost", 1234);
+                        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                        String name = fileToSend[0].getName();
+                        byte[] nameInBytes = name.getBytes();
+                        byte[] fileContentBytes = new byte[(int)fileToSend[0].length()];
+                        input.read(fileContentBytes);
+
+                        // first we send an integer to the server which is the length of the data that it will be receiving
+                        // The server then knows when it should stop expecting data from the client
+                        dataOutputStream.writeInt(nameInBytes.length);
+                        dataOutputStream.write(nameInBytes);
+
+                        dataOutputStream.writeInt(fileContentBytes.length);
+                        dataOutputStream.write(fileContentBytes);
+                    } catch (IOException error) {
+                        error.printStackTrace();
+                    }
+
+                }
+            }
+        });
     }
 }
